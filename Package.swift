c831fcc2b8821cbac9f87790cf0c7f68bfa26498
Francesco <cescofry@gmail.com>
2016-12-24 10:09:47 -0800

    Bump Kitura net and SwiftyJSON packages in the hope they will just work out with the rest of the app

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         Target(name: "AlchemyVision", dependencies: [.Target(name: "RestKit")])
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
     ]
 )
 
